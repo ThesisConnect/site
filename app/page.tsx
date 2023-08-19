7aa4b4534f8a85187a1ca7b8d92d63f8a5483e99
Paxwell-Paxwell <pompom231245@gmail.com>
2023-08-19 23:16:26 +0700

    add registe route in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import { redirect } from 'next/navigation'
 import Link from 'next/link'
 import Button from '@/components/login/Button';
+import Register from './register/page';
 
 export default function Home() {
   return (
     <div className=' h-44 flex flex-col justify-center items-center' >
       <Link  href={"/login"}>
-        <Button  type='button' className='w-24 bg-amber-300'   >
+        <Button  type='button' className='w-24 bg-green-300'   >
               Login
+        </Button>
+      </Link>
+      <Link  href={"/register"}>
+        <Button  type='button' className='w-24 bg-pink-500'   >
+              Register
         </Button>
       </Link>
     </div>
