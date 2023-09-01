4dba85e7eba3223ed49f536782fa2df9061fcb81
Paxwell-Paxwell <pompom231245@gmail.com>
2023-09-01 23:56:27 +0700

    test-format

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,12 +13,15 @@ const Sidebar = () => {
     return (
       <>
         <div className="overflow-auto w-20 h-[calc(100vh-62px)] p-2 bg-neutral-100 ">
+          
           <div
             className="flex flex-col justify-center items-center cursor-pointer"
             onClick={back}
           >
-            <MdArrowBackIosNew className="text-neutral-400 mt-3" size={40} />
-            <h2 className="text-xs font-semibold mt-1 text-neutral-400 ">
+               <MdArrowBackIosNew className="text-neutral-400 mt-3" size={40} />
+            <h2 className="text-xs font-semibold 
+            mt-1 
+            text-neutral-400 ">
               Back
             </h2>
           </div>
