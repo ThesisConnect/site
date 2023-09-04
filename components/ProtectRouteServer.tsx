9fbb341a62605f457dbc9e1c6115ae867dd97a55
Paxwell-Paxwell <pompom231245@gmail.com>
2023-09-04 23:26:10 +0700

    updateServerProtect

--- components/ProtectRouteServer.tsx
+++ components/ProtectRouteServer.tsx
@@ -1,13 +1,14 @@
 // helpers/protectRouteServer.js
 import axiosBaseurl from '@/config/baseUrl';
 
-const protectRouteServer = async () => {
+const protectRouteServer = async <T = any,>(
+  additionalProps?: () => Promise<T>,
+) => {
   try {
     const res = await axiosBaseurl.get('/auth/checkAuth', {
       withCredentials: true, // Forward the incoming request headers
     });
     const data = res.data;
-
     if (!data.isAuthenticated) {
       return {
         redirect: {
@@ -16,8 +17,9 @@ const protectRouteServer = async () => {
         },
       };
     }
+    const propsData = additionalProps ? await additionalProps() : {};
     return {
-      props: {}, // return empty props or add additional props if needed
+      props: propsData,
     };
   } catch (err) {
     // If an error occurs or the user isn't authenticated, redirect to login
@@ -31,3 +33,12 @@ const protectRouteServer = async () => {
 };
 
 export default protectRouteServer;
+// export const getServerSideProps = async (context) => {
+//     const userId = context.params?.id;
+  
+//     return await protectRouteServer(async () => {
+//       const userData = await getUserData(userId); // Using the userId directly here
+//       return { user: userData };
+//     });
+//   };
+  
