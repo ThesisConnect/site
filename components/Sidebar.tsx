6af3fc171ab5068a985a54efe1d3e1dc4ec21dcd
Nathaya-S <nathaya.ssn@gmail.com>
2023-09-07 10:46:58 +0700

    princesselle

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,7 @@
 import { usePathname } from 'next/navigation';
 import { MdArrowBackIosNew } from 'react-icons/md';
 import { useRouter } from 'next/navigation';
+import { FaHome } from 'react-icons/fa';
 
 const Sidebar = () => {
   const pathname = usePathname();
@@ -24,6 +25,22 @@ const Sidebar = () => {
             text-neutral-400 "
             >
               Back
+            </h2>
+          </div>
+        </div>
+      </>
+    );
+    if (pathname === '/summary' || pathname === '/plan')
+    return (
+      <>
+        <div className="overflow-auto w-20 h-[calc(100vh-62px)] p-2 bg-neutral-100 ">
+          <div
+            className="flex flex-col justify-center items-center cursor-pointer"
+            onClick={back}
+          >
+            <FaHome className="text-neutral-400 mt-3" size={40} />
+            <h2 className="text-xs font-semibold mt-1 text-neutral-400">
+              Home
             </h2>
           </div>
         </div>
