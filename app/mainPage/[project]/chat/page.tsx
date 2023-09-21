4ee7b44be0b335e4906fd91884fb5f7c629582dc
Paxwell-Paxwell <pompom231245@gmail.com>
2023-09-21 11:58:32 +0700

    update chat

--- app/mainPage/[project]/chat/page.tsx
+++ app/mainPage/[project]/chat/page.tsx
@@ -2,20 +2,27 @@
 import ChatLayout from '@/components/layout/ChatLayout';
 import { useSearchParams } from 'next/navigation';
 import MessageInput from '../../../../components/chat/MessageInput';
-import Message, { MessageProps } from '@/components/chat/Message';
+import Message, { FileCom, MessageProps } from '@/components/chat/Message';
 import { use, useEffect, useRef, useState } from 'react';
 import dynamic from 'next/dynamic';
 import LoadingNormal from '@/components/loading/LoadingNormal';
 import socket from '@/config/socket';
 import userStore from '@/stores/User';
 import { auth } from '@/config/firebase';
-
+import { ErrorBoundary } from 'react-error-boundary';
+import { divide } from 'lodash';
+interface fileContents {
+  file: File;
+  url: string;
+  path: string;
+  uid: string;
+}
 interface RecieveMessenger {
-  _id: string
-  uid: string
-  username: string
-  content: string | File
-  type: 'file' | 'text'
+  _id: string;
+  uid: string;
+  username: string;
+  content: string | FileCom;
+  type: 'file' | 'text';
 }
 const PageChat = () => {
   const searchParams = useSearchParams();
@@ -23,13 +30,14 @@ const PageChat = () => {
   const chatID = searchParams.get('chatID');
   const [chat, setChat] = useState<RecieveMessenger[]>([]);
   const [displayCount, setDisplayCount] = useState(30);
-  const [loading ,setLoadingChat] =useState(false);
+  const [loading, setLoadingChat] = useState(false);
   const devRef = useRef<HTMLDivElement>(null);
   const messageEndRef = useRef<HTMLDivElement>(null);
   const loadMoreRef = useRef(null);
   const handleInputHeightChange = (newHeight: number) => {
     if (messageEndRef.current) {
-      messageEndRef.current.scrollIntoView({});``
+      messageEndRef.current.scrollIntoView({});
+      ``;
     }
   };
   //download more content
@@ -39,7 +47,6 @@ const PageChat = () => {
     const observer = new IntersectionObserver(
       (entries) => {
         if (entries[0].isIntersecting && !initialRender) {
-          
           setDisplayCount((prevCount) => prevCount + 30);
         }
         initialRender = false;
@@ -66,11 +73,12 @@ const PageChat = () => {
     if (chatID) {
       socket.emit('join room', chatID);
 
-      socket.on('room messages', (newMessages:RecieveMessenger[]) => {
-        setChat(newMessages)
+      socket.on('room messages', (newMessages: RecieveMessenger[]) => {
+        console.log(newMessages);
+        setChat(newMessages);
       });
       socket.on('receive message', (newMessages) => {
-        setChat((prev)=>([...prev,newMessages]))
+        setChat((prev) => [...prev, newMessages]);
       });
       socket.on('error', (error) => {
         console.error('Socket error:', error);
@@ -87,11 +95,24 @@ const PageChat = () => {
     };
   }, [chatID]);
 
-  const handleSendData = (message: string) => {
+  const handleSendData = (message: string | fileContents) => {
     const chatIDFromURL = searchParams.get('chatID');
     if (chatIDFromURL) {
-      socket.emit('send message', chatIDFromURL, message);
-      
+      if (typeof message === 'string')
+        socket.emit('send message', chatIDFromURL, message);
+      else {
+        const typeArr = message.file.type.split('/');
+        const fileSent = {
+          name: message.file.name,
+          fileID: message.uid,
+          size: message.file.size,
+          type_file: typeArr[typeArr.length - 1],
+          lastModified: message.file.lastModified,
+          link: message.url,
+          memo: '',
+        };
+        socket.emit('send message', chatIDFromURL, fileSent);
+      }
     } else {
       console.error('Chat ID not found in URL');
     }
@@ -103,12 +124,18 @@ const PageChat = () => {
           <div className="p-4">
             <div ref={loadMoreRef}></div>
             {chat?.slice(-displayCount).map((msg, index) => (
-              <Message
-                key={index}
-                username={msg.username}
-                content={msg.content as string}
-                isOwnMessage={auth.currentUser?.uid === msg.uid}
-              />
+              <ErrorBoundary key={index} fallback={<div>error</div>}>
+                <Message
+                  username={msg.username}
+                  content={
+                    typeof msg.content === 'object'
+                      ? (msg.content as FileCom)
+                      : (msg.content as string)
+                  }
+                  type={typeof msg.content === 'object' ? 'file' : 'text'}
+                  isOwnMessage={auth.currentUser?.uid === msg.uid}
+                />
+              </ErrorBoundary>
             ))}
             {/* <Message
               username="Paxwell"
@@ -132,7 +159,6 @@ const PageChat = () => {
           <MessageInput
             handleInputHeightChange={handleInputHeightChange}
             onClickSend={handleSendData}
-
           />
         </div>
       </div>
